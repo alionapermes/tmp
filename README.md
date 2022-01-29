2e8d041c2852e5657d4014d18bf516aedd0a2ba2
alionapermes <alionapermes@gmail.com>
2022-01-29 19:38:11 +0700

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# tmp
+
+## about
+
+Temp repo for simple game server
+
+## build
+
+```sh
+mkdir build
+cd build
+cmake .. -DDEBUG=[ON;OFF], -DTESTS=[ON;OFF]
+make
+```
+
---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+
+TEST(suit, name)
+{
+    // todo...
+}
+
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
