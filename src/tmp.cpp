f378b3864a2778695c55a40411ceb3e9f85923cc
alionapermes <alionapermes@gmail.com>
2022-01-30 18:24:10 +0700

    add authorization

---
+++ src/tmp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+
+//#include "server.hpp"
+
+/* #define DEFAULT_CONFIG "tmp.cfg" */
+
+namespace opt = boost::program_options;
+
+
+int main(int argc, char** argv)
+{
+    /* std::string config_path = DEFAULT_CONFIG; */
+    /* std::string db_path; */
+    /* opt::variables_map vm; */
+    /* opt::options_description desc("Options"); */
+
+    /* desc.add_options()( */
+    /*     "config,c", */
+    /*     opt::value<std::string>(), */
+    /*     "Path to server config file" */
+    /* ); */
+
+    /* opt::store(opt::parse_command_line(argc, argv, desc), vm); */
+    /* config_path = vm["config"].as<std::string>(); */
+
+    return 0;
+}
+
